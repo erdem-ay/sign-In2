d8155531cc8c2dee338bc9774c9f84daafc3d5e4
erdem ay <eirdemmoon@gmail.com>
2022-10-10 18:29:16 +0300

    add sıgnın2

---
+++ main.js
@@ -0,0 +1,15 @@
+let x = document.querySelector('#Login');
+let y = document.querySelector('#Register');
+let z = document.querySelector('#btn');
+
+function register(){
+    x.style.left="-400px";
+    y.style.left="50px";
+    z.style.left="110px";
+}
+
+function login(){
+    x.style.left="50px";
+    y.style.left="450px";
+    z.style.left="0px";
+}
